c582049ea6e20b0542492b10c15b8ffc0610ab0f
Erika Ledesma <erika.jasmine06@gmail.com>
2023-02-25 19:43:28 -0800

    added to app.js and created .js and .css for recipe tile

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import Axios from "axios";
 import { useState } from "react";
 
 function App() {
-  const [query, setquery] = useState("")
+  const [query, setquery] = useState("");
+  const [recipes, setrecipes] = useState([]);
+
 
   const YOUR_APP_ID = "8d941e23";
   const YOUR_APP_KEY = "6ba4ea10183b47bb358513d52b0ac743";
@@ -13,6 +15,7 @@ function App() {
 
   async function getRecipes(){
     var result = await Axios.get(url);
+    setrecipes(result.data.hits);
     console.log(result.data);
   }
 
@@ -29,7 +32,15 @@ function App() {
         value={query} onChange={(e) => setquery(e.target.value)}/>
         <input className="app_submit" type="submit" value="Search"/>
       </form>
+
+      <div>
+        {recipes.map(recipe => {
+          return <RecipeTile/>
+        })}
+
+      </div>
     </div>
+    
   );
 }
 
